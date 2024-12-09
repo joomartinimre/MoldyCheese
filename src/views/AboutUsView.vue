47af5572597688ce7bb36dc5bd2b6203fa1b884b
BMarkAdrian <78158565+BMarkAdrian@users.noreply.github.com>
2024-12-09 11:39:23 +0100

    desing xd

---
+++ src/views/AboutUsView.vue
@@ -0,0 +1,6 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+<h1>About us</h1>
+</template>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+    <v-layout class="rounded rounded-md">
+      <v-app-bar title="🧀 Moldy Cheese"></v-app-bar>
+  
+      <v-navigation-drawer>
+        <v-list>
+          <v-list-item title="Navigation drawer"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+  
+      <v-main class="d-flex align-center justify-center" style="min-height: 300px;">
+        <RouterView></RouterView>
+      </v-main>
+    </v-layout>
+  </template>
