5967402ce3ff6d116497b6dd074eb29aee3a2e16
Ballon Márk Adrián <ballon.mark@ckik.loc>
2025-01-06 10:54:32 +0100

    helyek megkezdese

--- Vue/src/libs/vuetify.ts
+++ Vue/src/libs/vuetify.ts
@@ -3,6 +3,7 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import colors from 'vuetify/util/colors'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 
 const vuetify = createVuetify({
   components,
