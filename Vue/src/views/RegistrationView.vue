09b2bd7102726945779f5be0bc707de5647b8b82
Martin <joomartinimre@gmail.com>
2025-02-19 18:01:20 +0100

    merge utan

--- Vue/src/views/RegistrationView.vue
+++ Vue/src/views/RegistrationView.vue
@@ -117,22 +117,4 @@ const registerUser = async () => {
 
 </template>
 
-<style>
-input {
-  width: 100%;
-  padding: 8px;
-  margin: 5px 0;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-}
-button {
-  width: 100%;
-  padding: 10px;
-  background-color: blue;
-  color: white;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-</style>
 
